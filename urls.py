8cbc0a97bb8adfd60d793dc0c77ee2fa989fb08d
Aryan Sansi <aryansansi123@gmail.com>
2024-08-20 10:14:43 +0530

    updated urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-RTSP_URLS = "rtsp://username:password@IP-address/streamname/channel-number"
+RTSP_URLS = "rtsp://admin:admin123@192.168.1.153:554/avstream/channel=1/stream=0.sdp"
 
 def get_camera_url(index):
     return RTSP_URLS[index]
